447aef1bc3b1aa54d7ac36761f306fcd582bc062
Andrey Pedro Lefkum <andreypedro@MacBook-Pro-3.local>
2024-10-22 16:53:04 -0400

    FIX: Blog listing page with its respective tags was not created. Now it's fixed. The username of the user was also modified as requested.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Post
+
+def blog_list(request):
+    posts = Post.objects.all()
+    return render(request, 'core/blog_list.html', {'posts': posts})
